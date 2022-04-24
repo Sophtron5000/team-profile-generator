2a8cb384e98b493325104de7aa164ac8745fe8a3
Sophia C <95388964+Sophtron5000@user.noreply.github.com>
2022-04-24 16:06:46 -0700

    troubleshoot employee test

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -1,1 +1,14 @@
-const Employee = require("../lib")
+const Employee = require("../lib/employee")
+
+describe(Employee, () => {
+    describe("initialize", () => {
+        it("should start prompted questions", () => {
+           const prompt = new prompt; 
+           const question = prompt.message
+           const mock = jest.spyOn(prompt, "message")
+           mock.mockImplementation(() => {});
+
+           
+        })
+    })
+});
